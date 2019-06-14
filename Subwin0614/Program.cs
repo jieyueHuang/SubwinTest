96c893e050bf2cb322f857c31370bf8a7d76234e
540627754 <540627754@qq.com>
2019-06-14 16:15:41 +0800

    添加项目文件。

---
+++ Subwin0614/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Subwin0614
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
